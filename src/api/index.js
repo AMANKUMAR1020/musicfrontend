c01b9485b2508d3fa1bee9d28d55ff7ae5838c39
amankumar1020 <amankumarchouhan1020@gmail.com>
2024-06-27 21:26:02 +0530

    sixth1 commit

--- src/api/index.js
+++ src/api/index.js
@@ -4,16 +4,9 @@ export const client = axios.create({
 	baseURL: "https://musicbackend-bsf4.onrender.com/api",
 	// baseURL: "http://localhost:3500/api",
 	// baseURL: "https://musicbackend-jet.vercel.app/api",
-<<<<<<< HEAD
 	//	baseURL: 'https://musicbackend-jet.vercel.app/api',
 	//	baseURL: 'https://musicfrontend-amans-projects-9f0222ca.vercel.app/api'
 	//	baseURL: 'https://musicfrontend-git-main-amans-projects-9f0222ca.vercel.app/api'
-=======
-	baseURL: "https://musicbackend-bsf4.onrender.com/api",
-//	baseURL: 'https://musicbackend-jet.vercel.app/api',
-//	baseURL: 'https://musicfrontend-amans-projects-9f0222ca.vercel.app/api'
-//	baseURL: 'https://musicfrontend-git-main-amans-projects-9f0222ca.vercel.app/api'
->>>>>>> 2c71748628b28f44bb032c19a1d0166081b0d253
 });
 
 
